f2dd02ea9d07cbd66792be39723e206aff81010a
VijayaKumar Reddypogu <rvijay.net@gmail.com>
2021-07-31 15:21:30 +0530

    Add project files.

---
+++ WebAppMvcRDSSiteTwo/Context/mypostgresdbContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using WebAppMvcRDSSiteTwo.Models;
+
+#nullable disable
+
+namespace WebAppMvcRDSSiteTwo.Context
+{
+    public partial class mypostgresdbContext : DbContext
+    {
+        public mypostgresdbContext()
+        {
+        }
+
+        public mypostgresdbContext(DbContextOptions<mypostgresdbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Sale> Sales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                //optionsBuilder.UseNpgsql("Host=database-2.cqaxyfagdlth.us-east-2.rds.amazonaws.com;Database=mypostgresdb;Username=postgres;Password=Jedi2020;port=5432;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "en_US.UTF-8");
+
+            modelBuilder.Entity<Sale>(entity =>
+            {
+                entity.ToTable("sales");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Amount)
+                    .HasPrecision(18, 2)
+                    .HasColumnName("amount");
+
+                entity.Property(e => e.Productname)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("productname");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ WebAppMvcRDSSiteTwo/Models/Sale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WebAppMvcRDSSiteTwo.Models
+{
+    public partial class Sale
+    {
+        public int Id { get; set; }
+        public string Productname { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
